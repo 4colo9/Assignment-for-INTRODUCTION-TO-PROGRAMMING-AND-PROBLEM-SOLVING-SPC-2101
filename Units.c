312ef6cd82427ef77f34d04e7ba4e95f668503b9
Coleen Nicolette <reilintreasure@gmail.com>
2025-09-19 02:26:55 +0300

    Added units c file

---
+++ Units.c
@@ -0,0 +1,20 @@
+//Units for year 1 semester 1
+/*
+NAME:COLEEN NICOLETTE KANYANYA
+REG NO:PA106/G/28764/25.
+Description:Program to display the units for software engineering students for year 1 semester 1
+*/
+#include<stdio.h>//Preproccessor directive
+
+//main function
+int main() { 
+printf("Mathematics for science (SPM 2104) \n");
+printf("Fundamentals of ICT (UCU 2105) \n");
+printf("Computer system and organisation (SPI 2102) \n");
+printf("Communication skills (UCU 2101) \n");
+printf("Fundamentals of system analysis and design (SSE 2101)  \n");
+printf("Introduction to programming and problem solving (SPC 2101) \n");
+printf("Discrete Mathematics (SPM 2100) \n");
+
+return 0;
+}
