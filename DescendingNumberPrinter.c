253d82ba7996c49836e7ddb65d4d26dca84f269d
4colo9 <shawnwakhu@gmail.com>
2025-10-07 00:20:35 +0300

    Add files via upload

---
+++ DescendingNumberPrinter.c
@@ -0,0 +1,14 @@
+ /*
+NAME:COLEEN NICOLETTE KANYANYA
+REG NO:PA106/G/28764/25.
+Description:Program to print numbers from 100 to 50 in descinding order
+*/
+#include<stdio.h>
+
+int main(){
+    int n;
+    for(n=100;n>=50;n--){
+        printf("%d\n",n);
+    }
+    return 0;
+}
